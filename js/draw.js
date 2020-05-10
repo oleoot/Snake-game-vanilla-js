1c1be06e3f6502dbaaee61f7ac2312ff53af51e0
oleoot <oleoot28@gmail.com>
2020-05-10 20:12:18 +0300

    Displaying square on canvas

---
+++ js/draw.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+
+
+let snake;
+
+
+(function setup() {
+    snake = new Snake();
+    snake.draw();
+}());
---
+++ js/snake.js
@@ -0,0 +1,10 @@
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+
+    this.draw = function () {
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(this.x, this.y, scale, scale);
+    }
+
+}
