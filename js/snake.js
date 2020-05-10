520202f1769e466b76030034549e177411ed069b
oleoot <oleoot28@gmail.com>
2020-05-10 20:28:10 +0300

    Moving out of canvas bug fixed

--- js/snake.js
+++ js/snake.js
@@ -11,6 +11,18 @@ function Snake() {
     this.update = function () {
         this.x += this.xSpeed;
         this.y += this.ySpeed;
+        if (this.x > canvas.width) {
+            this.x = 0;
+        }
+        if (this.y > canvas.height) {
+            this.y = 0;
+        }
+        if (this.x < 0) {
+            this.x = canvas.width;
+        }
+        if (this.y < 0) {
+            this.y = canvas.height;
+        }
     }
     this.changeDirection = function (direction) {
         switch (direction) {
